0f1f179d3b8efb422925d3beffd5fa30d646b50c
Pascal Wengerter <pascal@wengerter.info>
2020-04-07 00:04:44 +0200

    Improve solution for challenge #1 and finish challenge #2

--- challenges/euler_1.exs
+++ challenges/euler_1.exs
@@ -1,20 +1,25 @@
-defmodule Solver do
-  def filter_range do
-    Range.new(3,999)
-    |> Enum.filter(fn x -> (rem(x, 3) == 0) or (rem(x, 5) == 0) end)
-  end
+# # Old solution
+# defmodule Solver do
+#   def filter_range do
+#     Range.new(3,999)
+#     |> Enum.filter(fn x -> (rem(x, 3) == 0) or (rem(x, 5) == 0) end)
+#   end
+#
+#   def sum_list([head | tail], acc) do
+#     sum_list(tail, acc + head)
+#   end
+#
+#   def sum_list([], acc) do
+#     acc
+#   end
+# end
+#
+# Solver.filter_range
+# |> Solver.sum_list(0)
+# |> IO.inspect(label: "This is the number you are looking for")
 
-  def sum_list([head | tail], acc) do
-    sum_list(tail, acc + head)
-  end
-
-  def sum_list([], acc) do
-    acc
-  end
-end
-
-Solver.filter_range
-|> Solver.sum_list(0)
-|> Integer.to_string
-|> Kernel.<>(" is the number you are looking for.")
-|> IO.puts
+# New solution
+Range.new(3,999)
+|> Enum.filter(fn x -> (rem(x, 3) == 0) or (rem(x, 5) == 0) end)
+|> Enum.reduce(0, fn i, acc -> i + acc end)
+|> IO.inspect(label: "This is the number you are looking for")
